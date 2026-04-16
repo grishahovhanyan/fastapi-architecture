1da271acd58f910f2c7d01255c4baefd8fa014f3
Grisha Hovhanyan <grisha.hovhanyann@gmail.com>
2026-04-16 14:17:22 +0400

    fixed custom base schema

--- app/utils/schemas.py
+++ app/utils/schemas.py
@@ -1,15 +1,9 @@
-from datetime import datetime
-from decimal import Decimal
-
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 
 
 class CustomBaseModel(BaseModel):
-  class Config:
-    alias_generator = to_camel
-    populate_by_name = True
-    json_encoders = {
-      datetime: lambda v: v.isoformat(),
-      Decimal: float,
-    }
+  model_config = ConfigDict(
+    alias_generator=to_camel,
+    populate_by_name=True
+  )
